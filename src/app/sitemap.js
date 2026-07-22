1e845cc427804da1ed1dd33278ed299edc5d33d7
Adil Khan <adilkhansumbal7@gmail.com>
2026-07-22 13:12:33 +0500

    sitemap.js change

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,19 +1,10 @@
 export default async function sitemap() {
   const baseUrl = "https://iamaadilkhan.vercel.app";
 
-  // Static pages
-  const staticPages = [
+  return [
     { url: baseUrl, lastModified: new Date() },
     { url: `${baseUrl}/about`, lastModified: new Date() },
     { url: `${baseUrl}/projects`, lastModified: new Date() },
     { url: `${baseUrl}/contact`, lastModified: new Date() },
   ];
-
-  const projects = await getProjects();
-  const projectPages = projects.map((project) => ({
-    url: `${baseUrl}/projects/${project.slug}`,
-    lastModified: project.updatedAt,
-  }));
-
-  return [...staticPages, ...projectPages];
 }
